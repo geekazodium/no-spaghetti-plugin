a724b0990e873f9c77903547fe79cc6dfca5f086
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-09-24 20:33:15 -0400

    feature: write to file only if violation found, err out workflow

--- addons/no_spaghetti/workflow/execute_ns_checker.gd
+++ addons/no_spaghetti/workflow/execute_ns_checker.gd
@@ -6,13 +6,18 @@ var output: FileAccess;
 func _ready() -> void:
 	var ns_checker: SpaghettiChecker = SpaghettiChecker.new();
 	ns_checker.load_rules();
-	self.output = FileAccess.open("res://warnings_compact.txt",FileAccess.WRITE);
 	ns_checker.lint_warnings_generated.connect(self.write_warning_to_file);
 	ns_checker.check_program();
-	self.output.close();
+	if self.output != null:
+		self.output.close();
 	
 func write_warning_to_file(path: String, text: String, results: Array[RegExMatch]) -> void:
 	var converted_res: Array[Array] = [];
 	for r: RegExMatch in results:
 		converted_res.append([r.get_start(), r.get_end()]);
-	self.output.store_line(JSON.stringify([path, converted_res]));
+	self.write_output(JSON.stringify([path, converted_res]));
+
+func write_output(line: String) -> void:
+	if self.output == null:
+		self.output = FileAccess.open("res://warnings_compact.txt",FileAccess.WRITE);
+	self.output.store_line(line);
